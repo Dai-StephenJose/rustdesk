7f89a411624223496d7b5d6397bd658c690702bd
Dai-StephenJose <d670677773@gmail.com>
2025-06-07 19:20:50 +0800

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -996,7 +996,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://rap.zwyktech.cn:21114".to_owned()
+    "http://opu1.daish.eu.org:21114".to_owned()
 }
 
 #[inline]

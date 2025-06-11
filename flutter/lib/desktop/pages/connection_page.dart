8b4a95ab5c68f3990021814ae7817f209ccf9156
StephenJose_Dai <g670677773@gmail.com>
2025-06-11 09:41:06 +0800

    changename

--- flutter/lib/desktop/pages/connection_page.dart
+++ flutter/lib/desktop/pages/connection_page.dart
@@ -90,18 +90,7 @@ class _OnlineStatusWidgetState extends State<OnlineStatusWidget> {
                 Flexible(
                   child: InkWell(
                     onTap: onUsePublicServerGuide,
-                    child: Row(
-                      children: [
-                        Flexible(
-                          child: Text(
-                            translate('setup_server_tip'),
-                            style: TextStyle(
-                                decoration: TextDecoration.underline,
-                                fontSize: em),
-                          ),
-                        ),
-                      ],
-                    ),
+                    
                   ),
                 )
               ],
